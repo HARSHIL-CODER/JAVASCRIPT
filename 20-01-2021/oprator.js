c5d0be108d88e4124f26786b91ec10ea43950c3a
HARSHIL-CODER <77496341+HARSHIL-CODER@users.noreply.github.com>
2021-01-21 20:36:56 +0530

    this is my homework
    attached files oprator.html and oprator .js

---
+++ 20-01-2021/oprator.js
@@ -0,0 +1,18 @@
+let a = 81
+let b = 81
+let sum , sub , mul , rem , que;
+
+sum =  a + b;
+sub =  a - b;
+mul =  a * b;
+que =  a / b;
+
+console.log("a = "+a);
+console.log("b = "+b);
+console.log("sum ="+sum);
+console.log("sub ="+sub);
+console.log("mul ="+mul);
+console.log("rem ="+rem);
+
+console.log("Equality operator ex : "+ (a===b));
+console.log("Not Equal operator ex : "+ (a!==b));
