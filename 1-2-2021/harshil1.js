86029220185be04de7ccdcecea1425a265036727
HARSHIL-CODER <77496341+HARSHIL-CODER@users.noreply.github.com>
2021-02-02 14:58:56 +0530

    atttached file harshil1.html and harshil1.js
    all files attached

---
+++ 1-2-2021/harshil1.js
@@ -0,0 +1,206 @@
+alert("you are playing KON BANEGA ROADPATI")
+	alert ("PHELA PRASHAN 100 RUPPES KE LIYE")
+	alert ("WHO WAS THE FIRST PRESIDENT OF INDIA?")
+	const Q1 = prompt(" \n 1- DR.RAJENDER PRASAD\n 2- RAHUL GHANDI(JI)\n 3-MODI JI \n 4- SHINCHAN")
+	if (Q1 === '1'){
+		
+		alert("ADBHUT AAP JEET GAYE 100 RUPPES ")
+	}
+	if (Q1=== '2'){
+		
+		alert("YE KABHI BAN BHI NAHI SAKTA LOL ")
+	}
+	if (Q1 === '3'){
+		
+		alert("nope wrong nahi mele 100 rupee ")
+	}
+	if (Q1 === '4'){
+		
+		alert("nahi yaar phele hi har gaye ")
+	}
+
+	alert ("DUSRA PRASHAN -100 RUPPES KE LIYE")
+	alert ("Which of these must a player hold during a Raid in a game of Kabaddi?")
+	const Q2 = prompt(" \n 1- LAUGHTER\n 2- BALL\n 3-BREATH \n 4- sweat")
+	if (Q2 === '1'){
+		
+		alert("kabbadi hai joke nahi ")
+	}
+	if (Q2 === '2'){
+		
+		alert("football hai kya ,galat  ")
+	}
+	if (Q2 === '3'){
+		
+		alert("ye hui na baat u won ")
+	}
+	if (Q2 === '4'){
+		
+		alert("nahi yaar phele hi har gaye ,WRONG")
+	}
+	alert ("THIRD PRASHAN 500 RUPPES KE LIYE")
+	alert ("Who designed the Eiffel Tower?")
+	const Q3 = prompt(" \n 1- LEONADO DA VINCI\n 2-  Gustave Eiffel,\n 3-QUEEN VICTORIA \n 4- TOM AND JERRY")
+	if (Q3 === '1'){
+		
+		alert("PAINTING BANATA THA VO,WRONG ")
+	}
+	if (Q3 === '2'){
+		
+		alert("YE TO MUGHE BHI NAHI PATA THA , CORRECRT  ")
+	}
+	if (Q3 === '3'){
+		
+		alert("HA YE BHI BANA SAKTE THI BUT WRONG ")
+	}
+	if (Q3 === '4'){
+		
+		alert("YE TO BILKUL HI NAHI YE MAT SELECT KARNA ,WRONG ")
+	}
+	alert ("FOURTH PRASHAN -500 RUPPES KE LIYE")
+	alert ("the battle of Austerlitz also known as?")
+	const Q4 = prompt(" \n 1- WORLD WAR 1\n 2-I DONT NOW I DIDNT GOT TO SCHOOL ON TIME   \n 3-The Battle of the five Emperors\n 4- The Battle of the Three Emperors")
+	if (Q4 === '1'){
+		
+		alert("JUST LEAVE ")
+	}
+	if (Q4 === '2'){
+		
+		alert("PATA THA THABI FAIL H GAYE NA , WRONG  ")
+	}
+	if (Q4 === '3'){
+		
+		alert("NAHI YAAR 3 HAI SAHI ANSWER ,WRONG ")
+	}
+	if (Q4 === '4'){
+		
+		alert("WOW IMPOSSIBLE ,CORRECT")
+	}
+	alert ("FIFTH PRASHAN 1000 RUPPES KE LIYE")
+	alert ("What is Prince William's full name??")
+	const Q5 = prompt(" \n 1- william wordsworth\n 2-William Arthur Philip Louis Windsor \n 3-william shakespeare\n 4-William Butler Yeats")
+ 
+	if (Q5  === '1'){
+
+	alert("nope WRONG one")
+	}
+	if (Q5 === '2'){
+		
+		alert("WOW IMPOSSIBLE ,CORRECT ")
+	}
+	if (Q5 === '3'){
+		
+		alert("NAHI YAAR 2 HAI SAHI ANSWER ,WRONG ")
+	}
+	if (Q5 === '4'){
+		
+		alert("wrong ")
+	}
+		alert ("sixth PRASHAN -1000 RUPPES KE LIYE")
+	alert ("In the abbreviation PNR, used while issuing rail or air ticket, what does R stand for??")
+	const Q6 = prompt(" \n 1- Reservation\n 2-  Registration\n 3- Record\n 4- Recognition")
+ 
+	if (Q6  === '1'){
+
+	alert("nope WRONG one")
+	} 
+
+	if (Q6 === '2'){
+		
+		alert("nope ")
+	}
+	if (Q6 === '3'){
+		
+		alert("correct one ")
+	}
+	if (Q6 === '4'){
+		
+		alert("wrong ")
+	}
+		alert ("SEVENTH PRASHAN 2500 RUPPES KE LIYE")
+	alert ("Which is the only one of these which comes in different sizes called mini, micro and nano??")
+	const Q7 = prompt(" \n 1- cars\n 2-  truck\n 3- cards\n 4- sim caards")
+ 
+	if (Q7 === '1'){
+
+	alert("nope WRONG one")
+	}
+	
+	if (Q7 === '2'){
+		
+		alert("nope ")
+	}
+	if (Q7 === '3'){
+		
+		alert("wrong ")
+	}
+	if (Q7 === '4'){
+		
+		alert("correct")
+	}
+			alert ("eighth PRASHAN -2500 RUPPES KE LIYE")
+	alert ("Which global retailer bought 77% of Flipkart for over 16 billion US Dollar in 2018?")
+	const Q8 = prompt(" \n 1- walmart\n 2-  amazon\n 3- teaco\n 4- flipcart")
+ 
+	if (Q8 === '1'){
+
+	alert("correct")
+	}
+
+	if (Q8 === '2'){
+		
+		alert("nope ")
+	}
+	if (Q8 === '3'){
+		
+		alert("wrong ")
+	}
+	if (Q8 === '4'){
+		
+		alert("no no no wrong")
+	}
+		
+			alert ("ninth PRASHAN 10000 RUPPES KE LIYE")
+	alert ("In February 2017, ISRO created a world record by launching how many satellites through a single rocket??")
+	const  Q9 = prompt(" \n 1- 82\n 2- 69\n 3- 104\n 4- 55")
+ 
+	if (Q9 === '1'){
+
+	alert("nope")
+	}
+	if (Q9 === '2'){
+		
+		alert("wrong")
+	}
+	if (Q9 === '3'){
+		
+		alert("correct ")
+	}
+	if (Q9 === '4'){
+		
+		alert("no no no wrong")
+	}
+			
+			alert ("  tenth PRASHAN  - 10000 RUPPES KE LIYE")
+	alert ("Which is the largest joint in the human body??")
+	const Q10  = prompt(" \n 1- elbow\n 2- hip\n 3- shoulder \n 4- knee")
+ 
+	if (Q10 === '1'){
+
+	alert("nope")
+	}
+
+	if (Q10 === '2'){
+		
+		alert("wrong")
+	}
+	if (Q10 === '3'){
+		
+		alert("no no no wrong ")
+	}
+	if (Q10 === '4'){
+		
+		alert("correct")
+		
+	}
+	alert ("PAISE AAP PAYTM KARENGE YA FIR CARD SE ?")
